a0db53ffd67b0b734414739b27b36a76c94d2017
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-12-20 19:22:42 -0300

    feat: ensure UpdateCustomerUseCase updates and return UpdateCustomerDto

--- src/usecase/customer/update/update-customer-usecase.ts
+++ src/usecase/customer/update/update-customer-usecase.ts
@@ -1,12 +1,12 @@
 import { Address, CustomerRepositoryInterface } from '@/domain/customer'
-import { InputUpdateCustomerDto } from './update-customer-dto'
+import { InputUpdateCustomerDto, OutputUpdateCustomerDto } from './update-customer-dto'
 
 export class UpdateCustomerUseCase {
   constructor (
     private readonly customerRepository: CustomerRepositoryInterface
   ) {}
 
-  async run (input: InputUpdateCustomerDto): Promise<void> {
+  async run (input: InputUpdateCustomerDto): Promise<OutputUpdateCustomerDto> {
     const customer = await this.customerRepository.find(input.id)
     customer.changeName(input.name)
     customer.changeAddress(
@@ -19,5 +19,16 @@ export class UpdateCustomerUseCase {
     )
 
     this.customerRepository.update(customer)
+
+    return {
+      id: customer.id,
+      name: customer.name,
+      address: {
+        street: customer.address.street,
+        city: customer.address.city,
+        number: customer.address.number,
+        zipCode: customer.address.zipCode
+      }
+    }
   }
 }
