6386cdfebd05582527cd5a6231f162d70aee4076
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-12-20 19:45:10 -0300

    feat: ensure ListCustomerUseCase returns an OutputListCustomerDto

--- src/usecase/customer/list/list-customer-usecase.ts
+++ src/usecase/customer/list/list-customer-usecase.ts
@@ -1,11 +1,29 @@
-import { CustomerRepositoryInterface } from '@/domain/customer'
+import { Customer, CustomerRepositoryInterface } from '@/domain/customer'
+import { OutputListCustomerDto } from './list-customer-dto'
 
 export class ListCustomerUseCase {
   constructor (
     private readonly customerRepository: CustomerRepositoryInterface
   ) {}
 
-  async run (): Promise<void> {
-    this.customerRepository.findAll()
+  async run (): Promise<OutputListCustomerDto> {
+    const customers = await this.customerRepository.findAll()
+
+    return formatOutput(customers)
+  }
+}
+
+const formatOutput = (customer: Customer[]): OutputListCustomerDto => {
+  return {
+    customers: customer.map((customer) => ({
+      id: customer.id,
+      name: customer.name,
+      address: {
+        street: customer.address.street,
+        number: customer.address.number,
+        city: customer.address.city,
+        zipCode: customer.address.zipCode
+      }
+    }))
   }
 }
