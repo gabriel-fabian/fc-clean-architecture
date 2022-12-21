135d20719be4d0c9ff57341b7119067e4ab881fb
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-12-21 13:30:35 -0300

    feat: ensure UpdateProductUseCase update and return an OutputUpdateProductDto

--- src/usecase/product/update/update-product-usecase.ts
+++ src/usecase/product/update/update-product-usecase.ts
@@ -1,12 +1,22 @@
 import { ProductRepositoryInterface } from '@/domain/product'
-import { InputUpdateProductDto } from './update-product-dto'
+import { InputUpdateProductDto, OutputUpdateProductDto } from './update-product-dto'
 
 export class UpdateProductUseCase {
   constructor (
     private readonly productRepository: ProductRepositoryInterface
   ) {}
 
-  async run (input: InputUpdateProductDto): Promise<void> {
-    this.productRepository.find(input.id)
+  async run (input: InputUpdateProductDto): Promise<OutputUpdateProductDto> {
+    const product = await this.productRepository.find(input.id)
+    product.changeName(input.name)
+    product.changePrice(input.price)
+
+    await this.productRepository.update(product)
+
+    return {
+      id: product.id,
+      name: product.name,
+      price: product.price
+    }
   }
 }
