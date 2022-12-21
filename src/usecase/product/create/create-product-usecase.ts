bb47d73c30c261a24c18a0d4271179840ae377b9
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-12-21 12:55:27 -0300

    feat: ensure CreateProductUseCase create and return an OutputCreateProductDto

--- src/usecase/product/create/create-product-usecase.ts
+++ src/usecase/product/create/create-product-usecase.ts
@@ -1,14 +1,20 @@
 import { ProductFactory, ProductRepositoryInterface } from '@/domain/product'
-import { InputCreateProductDto } from './create-product-dto'
+import { InputCreateProductDto, OutputCreateProductDto } from './create-product-dto'
 
 export class CreateProductUseCase {
   constructor (
     private readonly productRepository: ProductRepositoryInterface
   ) {}
 
-  async run (input: InputCreateProductDto): Promise<void> {
+  async run (input: InputCreateProductDto): Promise<OutputCreateProductDto> {
     const product = ProductFactory.create(input.name, input.price)
 
     await this.productRepository.create(product)
+
+    return {
+      id: product.id,
+      name: product.name,
+      price: product.price
+    }
   }
 }
