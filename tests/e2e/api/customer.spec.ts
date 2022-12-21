860e3bb12f2e4253120023bf222fc32cf444f150
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-12-21 18:32:33 -0300

    test: ensure GET /customer returns a list of customer

--- tests/e2e/api/customer.spec.ts
+++ tests/e2e/api/customer.spec.ts
@@ -2,6 +2,8 @@ import { faker } from '@faker-js/faker'
 import request from 'supertest'
 
 import { app, sequelize } from '@/infra/api/express'
+import { CustomerRepository } from '@/infra/customer'
+import { mockCustomer } from '@/tests/domain/mocks'
 
 describe('Customer E2E', () => {
   beforeEach(async () => {
@@ -46,5 +48,25 @@ describe('Customer E2E', () => {
       })
 
     expect(response.status).toBe(500)
+  })
+
+  it('should list all customers', async () => {
+    const customer1 = mockCustomer()
+    const customer2 = mockCustomer()
+
+    const customerRepository = new CustomerRepository()
+    customerRepository.create(customer1)
+    customerRepository.create(customer2)
+
+    const response = await request(app)
+      .get('/customer')
+      .send()
+
+    expect(response.status).toBe(200)
+    expect(response.body.customers.length).toBe(2)
+    expect(response.body.customers[0].name).toBe(customer1.name)
+    expect(response.body.customers[0].address.street).toBe(customer1.address.street)
+    expect(response.body.customers[1].name).toBe(customer2.name)
+    expect(response.body.customers[1].address.street).toBe(customer2.address.street)
   })
 })
