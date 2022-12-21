f9952f9eb08a31f5df7b92adc7160e04dc212b05
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-12-21 11:36:17 -0300

    feat: ensure FindProductUseCase find a product and return a DTO

--- src/usecase/product/find/find-product-usecase.ts
+++ src/usecase/product/find/find-product-usecase.ts
@@ -1,12 +1,18 @@
 import { ProductRepositoryInterface } from '@/domain/product'
-import { InputFindProductDto } from './find-product-dto'
+import { InputFindProductDto, OutputFindProductDto } from './find-product-dto'
 
 export class FindProductUseCase {
   constructor (
     private readonly productRepository: ProductRepositoryInterface
   ) {}
 
-  async run (input: InputFindProductDto): Promise<void> {
-    this.productRepository.find(input.id)
+  async run (input: InputFindProductDto): Promise<OutputFindProductDto> {
+    const product = await this.productRepository.find(input.id)
+
+    return {
+      id: product.id,
+      name: product.name,
+      price: product.price
+    }
   }
 }
