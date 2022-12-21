afcbf5638275b583d6cf52384340b12c54c4a58f
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-12-21 18:32:15 -0300

    feat: ensure GET /customer returns a list of customer

--- src/infra/api/routes/customer-route.ts
+++ src/infra/api/routes/customer-route.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express'
 
 import { CustomerRepository } from '@/infra/customer'
-import { CreateCustomerUseCase } from '@/usecase/customer'
+import { CreateCustomerUseCase, ListCustomerUseCase } from '@/usecase/customer'
 
 export const customerRoute = express.Router()
 
@@ -9,7 +9,6 @@ export const customerRoute = express.Router()
 customerRoute.post('/', async (req: Request, res: Response) => {
   const usecase = new CreateCustomerUseCase(new CustomerRepository())
 
-  console.log(req.body)
   try {
     const customerDto = {
       name: req.body.name,
@@ -22,6 +21,19 @@ customerRoute.post('/', async (req: Request, res: Response) => {
     }
 
     const output = await usecase.run(customerDto)
+    res.send(output)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+customerRoute.get('/', async (_req: Request, res: Response) => {
+  const usecase = new ListCustomerUseCase(new CustomerRepository())
+
+  try {
+    const output = await usecase.run()
+
     res.send(output)
   } catch (err) {
     res.status(500).json(err)
