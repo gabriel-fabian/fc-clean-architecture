e954309cb59838b65b4bb0873640be542f414928
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-12-21 11:58:57 -0300

    refactor: remove unnecessary ProductB

--- src/domain/product/factory/product-factory.ts
+++ src/domain/product/factory/product-factory.ts
@@ -1,16 +1,9 @@
 import { v4 as uuid } from 'uuid'
 
-import { Product, ProductB, ProductInterface } from '@/domain/product'
+import { Product } from '@/domain/product'
 
 export default class ProductFactory {
-  public static create (type: string, name: string, price: number): ProductInterface {
-    switch (type) {
-      case 'a':
-        return new Product(uuid(), name, price)
-      case 'b':
-        return new ProductB(uuid(), name, price)
-      default:
-        throw new Error('Invalid product type')
-    }
+  public static create (name: string, price: number): Product {
+    return new Product(uuid(), name, price)
   }
 }
--- src/domain/product/index.ts
+++ src/domain/product/index.ts
@@ -1,5 +1,4 @@
 export { default as Product } from './entity/product'
-export { default as ProductB } from './entity/product-b'
 export { default as ProductInterface } from './entity/product-interface'
 export { default as ProductCreatedEvent } from './event/product-created-event'
 export { default as ProductFactory } from './factory/product-factory'
