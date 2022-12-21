5e35d902a6d292c0deb47846bbf37b60bcf96e5b
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-12-21 13:54:03 -0300

    test: ensure ListProductUseCase returns an OutputListProductDto

--- tests/usecase/product/list/list-produce-usecase.spec.ts
+++ tests/usecase/product/list/list-produce-usecase.spec.ts
@@ -1,5 +1,6 @@
 import { ListProductUseCase } from '@/usecase/product'
 import { ProductRepositorySpy } from '@/tests/infra/mocks/mock-product-repository'
+import { mockProduct } from '@/../tests/domain/mocks'
 
 type SutTypes = {
   sut: ListProductUseCase
@@ -23,5 +24,31 @@ describe('ListProductUseCase', () => {
     await sut.run()
 
     expect(productRepositorySpy.callsCount).toBe(1)
+  })
+
+  it('should return an OutputListProductDto', async () => {
+    const { sut, productRepositorySpy } = makeSut()
+
+    const product1 = mockProduct()
+    const product2 = mockProduct()
+
+    jest.spyOn(productRepositorySpy, 'findAll').mockResolvedValueOnce([product1, product2])
+
+    const output = await sut.run()
+
+    expect(output).toEqual({
+      products: [
+        {
+          id: product1.id,
+          name: product1.name,
+          price: product1.price
+        },
+        {
+          id: product2.id,
+          name: product2.name,
+          price: product2.price
+        }
+      ]
+    })
   })
 })
