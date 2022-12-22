8a3e6566f072639a2b3ef39f6b6cd5df7e9a2a01
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-12-22 14:29:12 -0300

    feat(notification): ensure Customer extends Entity

--- src/domain/customer/entity/customer.ts
+++ src/domain/customer/entity/customer.ts
@@ -1,25 +1,37 @@
+import { Entity } from '@/domain/@shared/entity/entity-abstract'
+import { NotificationError } from '@/domain/@shared/notification'
 import { Address } from '..'
 
-export default class Customer {
-  private readonly _id: string
+export default class Customer extends Entity {
   private _name: string = ''
   private _address!: Address
   private _active: boolean = false
   private _rewardPoints: number = 0
 
   constructor (id: string, name: string) {
+    super()
     this._id = id
     this._name = name
     this.validate()
+
+    if (this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.errors)
+    }
   }
 
   validate (): void {
-    if (this._id.length === 0) {
-      throw new Error('Id is required')
+    if (this.id.length === 0) {
+      this.notification.addError({
+        context: 'customer',
+        message: 'Id is required'
+      })
     }
 
     if (this._name.length === 0) {
-      throw new Error('Name is required')
+      this.notification.addError({
+        context: 'customer',
+        message: 'Name is required'
+      })
     }
   }
 
@@ -29,10 +41,6 @@ export default class Customer {
 
   get rewardPoints (): number {
     return this._rewardPoints
-  }
-
-  get id (): string {
-    return this._id
   }
 
   get address (): Address {
