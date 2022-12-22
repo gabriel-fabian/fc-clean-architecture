d4872a4e754d3692d1ec69d99e67a1a36c2bd7d9
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2022-12-22 15:24:49 -0300

    feat: ensure Product throws NotificationError when params are invalid

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,29 +1,44 @@
 import ProductInterface from './product-interface'
+import { Entity } from '@/domain/@shared/entity/entity-abstract'
+import { NotificationError } from '@/domain/@shared/notification'
 
-export default class Product implements ProductInterface {
-  private readonly _id: string
+export default class Product extends Entity implements ProductInterface {
   private _name: string
   private _price: number
 
   constructor (id: string, name: string, price: number) {
+    super()
     this._id = id
     this._name = name
     this._price = price
 
     this.validate()
+
+    if (this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.errors)
+    }
   }
 
   validate (): boolean {
     if (this._id.length === 0) {
-      throw new Error('Id is required')
+      this.notification.addError({
+        message: 'Id is required',
+        context: 'product'
+      })
     }
 
     if (this._name.length === 0) {
-      throw new Error('Name is required')
+      this.notification.addError({
+        message: 'Name is required',
+        context: 'product'
+      })
     }
 
     if (this._price <= 0) {
-      throw new Error('Price must be valid')
+      this.notification.addError({
+        message: 'Price must be valid',
+        context: 'product'
+      })
     }
 
     return true
@@ -37,10 +52,6 @@ export default class Product implements ProductInterface {
   changePrice (price: number): void {
     this._price = price
     this.validate()
-  }
-
-  get id (): string {
-    return this._id
   }
 
   get name (): string {
